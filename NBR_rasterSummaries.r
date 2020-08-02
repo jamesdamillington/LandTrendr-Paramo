5fd4ab1132f2b49797cf03cadff9e5c4b9558883
James Millington <jamesdamillington@gmail.com>
2020-08-02 10:57:01 +0100

    rename and tidying

--- NBR_rasterSummaries.r
+++ NBR_rasterSummaries.r
@@ -255,59 +255,5 @@ for(scenario in scenario_list){
 }  
   
   
-  
-  
-  
-  
-  
-#   
-#   
-# #Try using https://googledrive.tidyverse.org/ to read files
-# library(googledrive)
-# 
-# #options(gargle_quiet = FALSE)
-# #drive_auth(use_oob = TRUE)
-# 
-# drive_find(n_max = 30)
-# 
-# drive_download("~/Research/GroundTruthingBuffer500m_mask_trim.tif",
-#                path = "Data/NBRanalysis/GroundTruthingBuffer500m_mask_trim.tif")
-# 
-# drive_download("~/Research/LTR_2020_Final/LTR_AllYear_8.tif",
-#                path = "Data/NBRanalysis/LTR_AllYear_8.tif")
-# 
-# drive_find(pattern="LTR_2020_Final/LTR_AllYear_8.tif")$id
-# 
-# 
-# 
-# 
-# #below with help from https://community.rstudio.com/t/how-to-download-a-google-drives-contents-based-on-drive-id-or-url/16896/12
-# #authentication "went stale" after a while and I had to restart R session. Try this fix: https://github.com/tidyverse/googledrive/issues/304
-# 
-# #Kris' shared folder URL
-# folder_url <- "https://drive.google.com/drive/folders/1CmGmXPGjhwrSnVY0VhSPRmQ8u2xYwYHA"
-# 
-# folder <- drive_get(as_id(folder_url))
-# 
-# folder_ls <- drive_ls(folder)  #get list of all files in the shared folder
-# 
-# folder_ls <- drive_ls(drive_get(as_id(folder_url)))
-# 
-# #start scenario and file loop here
-# 
-# scenario_id <- 8
-# window <- "AllYear"
-# 
-# file_name <- paste0("LTR_",window,"_",scenario_id,".tif")
-# file_id <- folder_ls$id[folder_ls$name == file_name]  #get the id of the file we want (this can be pasted into a URL)
-# 
-# #use the id to download file from URL
-# drive_download(paste0("https://drive.google.com/file/d/",file_id), 
-#                path = paste0("Data/NBRanalysis/",file_name))
-# 
-# #delete file
-# unlink(paste0("Data/NBRanalysis/",file_name))
-# 
-# 
 
 
