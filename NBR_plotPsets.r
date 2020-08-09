084af70f612175173f35be2eb87cbf3cb653b891
jamesdamillington <jamesdamillington@gmail.com>
2020-08-09 10:14:59 +0100

    more flexibility with ltrvar

--- NBR_plotPsets.r
+++ NBR_plotPsets.r
@@ -4,84 +4,86 @@ library(readr)
 library(ggplot2) 
 library(dplyr)
 
-data_dir <- paste0(getwd(),"/Data/NBRanalysis/")  ## define the name of directory to save results
+data_dir <- paste0(getwd(),"/Data/NBRanalysis/AllPixels/")  ## define the name of directory to save results
+
+ltrvar <- "PreValue"
 
 W <- read_csv(paste0(data_dir,"W_Summary_all.csv"))
 YW <- read_csv(paste0(data_dir,"YW_Summary_all.csv"))
 WP <- read_csv(paste0(data_dir,"WP_Summary_all.csv"))
 
-pdf(paste0(data_dir, "PreVal_Window.pdf"), width=10, height=10)
+pdf(paste0(data_dir, ltrvar, "_Window.pdf"), width=10, height=10)
   
 W %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Window = recode(Window, All = "All Year", Third = "Jan-Apr")) %>%
   filter(Pset <= 48) %>%
   mutate(Pset = factor(Pset)) %>%
   ggplot(aes(Pset, med, colour=Window)) +
   geom_pointrange(aes(ymin=q5, ymax=q95),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (median with 5th and 95th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (median with 5th and 95th percentiles)")) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 W %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Window = recode(Window, All = "All Year", Third = "Jan-Apr")) %>%
   filter(Pset > 48 & Pset <= 96) %>%
   mutate(Pset = factor(Pset)) %>%
   ggplot(aes(Pset, med, colour=Window)) +
   geom_pointrange(aes(ymin=q5, ymax=q95),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (median with 5th and 95th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (median with 5th and 95th percentiles)")) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 W %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Window = recode(Window, All = "All Year", Third = "Jan-Apr")) %>%
   filter(Pset > 96) %>%
   mutate(Pset = factor(Pset)) %>%
   ggplot(aes(Pset, med, colour=Window)) +
   geom_pointrange(aes(ymin=q5, ymax=q95),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (median with 5th and 95th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (median with 5th and 95th percentiles)")) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 W %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Window = recode(Window, All = "All Year", Third = "Jan-Apr")) %>%
   filter(Pset <= 48) %>%
   mutate(Pset = factor(Pset)) %>%
   ggplot(aes(Pset, mean, colour=Window)) +
   geom_pointrange(aes(ymin=q25, ymax=q75),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (mean with 25th and 75th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (mean with 25th and 75th percentiles)")) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 W %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Window = recode(Window, All = "All Year", Third = "Jan-Apr")) %>%
   filter(Pset > 48 & Pset <= 96) %>%
   mutate(Pset = factor(Pset)) %>%
   ggplot(aes(Pset, mean, colour=Window)) +
   geom_pointrange(aes(ymin=q25, ymax=q75),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (mean with 25th and 75th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (mean with 25th and 75th percentiles)")) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 W %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Window = recode(Window, All = "All Year", Third = "Jan-Apr")) %>%
   filter(Pset > 96) %>%
   mutate(Pset = factor(Pset)) %>%
   ggplot(aes(Pset, mean, colour=Window)) +
   geom_pointrange(aes(ymin=q25, ymax=q75),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (mean with 25th and 75th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (mean with 25th and 75th percentiles)")) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 
@@ -89,47 +91,47 @@ W %>%
 
 
 YW %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Window = recode(Window, All = "All Year", Third = "Jan-Apr")) %>%
   filter(Pset <= 48) %>%
   mutate(Pset = factor(Pset)) %>%
   ggplot(aes(Pset, med, colour=Window)) +
   geom_pointrange(aes(ymin=q5, ymax=q95),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (median with 5th and 95th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (median with 5th and 95th percentiles)")) +
   facet_grid(Year~.) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 YW %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Window = recode(Window, All = "All Year", Third = "Jan-Apr")) %>%
   filter(Pset > 48 & Pset <= 96) %>%
   mutate(Pset = factor(Pset)) %>%
   ggplot(aes(Pset, med, colour=Window)) +
   geom_pointrange(aes(ymin=q5, ymax=q95),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (median with 5th and 95th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (median with 5th and 95th percentiles)")) +
   facet_grid(Year~.) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 YW %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Window = recode(Window, All = "All Year", Third = "Jan-Apr")) %>%
   filter(Pset > 96) %>%
   mutate(Pset = factor(Pset)) %>%
   ggplot(aes(Pset, med, colour=Window)) +
   geom_pointrange(aes(ymin=q5, ymax=q95),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (median with 5th and 95th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (median with 5th and 95th percentiles)")) +
   facet_grid(Year~.) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 
 WP %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Window = recode(Window, All = "All Year", Third = "Jan-Apr")) %>%
   mutate(Paramo = as.character(Paramo)) %>%
   mutate(Paramo = recode(Paramo, "0" = "Outside", "1" = "Inside")) %>%
@@ -138,13 +140,13 @@ WP %>%
   ggplot(aes(Pset, med, colour=Window)) +
   geom_pointrange(aes(ymin=q5, ymax=q95),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (median with 5th and 95th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (median with 5th and 95th percentiles)")) +
   facet_grid(.~Paramo) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 WP %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Window = recode(Window, All = "All Year", Third = "Jan-Apr")) %>%
   mutate(Paramo = as.character(Paramo)) %>%
   mutate(Paramo = recode(Paramo, "0" = "Outside", "1" = "Inside")) %>%
@@ -153,13 +155,13 @@ WP %>%
   ggplot(aes(Pset, med, colour=Window)) +
   geom_pointrange(aes(ymin=q5, ymax=q95),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (median with 5th and 95th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (median with 5th and 95th percentiles)")) +
   facet_grid(.~Paramo) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 WP %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Window = recode(Window, All = "All Year", Third = "Jan-Apr")) %>%
   mutate(Paramo = as.character(Paramo)) %>%
   mutate(Paramo = recode(Paramo, "0" = "Outside", "1" = "Inside")) %>%
@@ -168,8 +170,8 @@ WP %>%
   ggplot(aes(Pset, med, colour=Window)) +
   geom_pointrange(aes(ymin=q5, ymax=q95),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (median with 5th and 95th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (median with 5th and 95th percentiles)")) +
   facet_grid(.~Paramo) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
@@ -183,10 +185,10 @@ dev.off()
 P <- read_csv(paste0(data_dir,"P_Summary_all.csv"))
 YP <- read_csv(paste0(data_dir,"YP_Summary_all.csv"))
 
-pdf(paste0(data_dir, "PreVal_Paramo.pdf"), width=10, height=10)
+pdf(paste0(data_dir, ltrvar, "_Paramo.pdf"), width=10, height=10)
 
 P %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Paramo = as.character(Paramo)) %>%
   mutate(Paramo = recode(Paramo, "0" = "Outside", "1" = "Inside")) %>%
   filter(Pset <= 48) %>%
@@ -194,12 +196,12 @@ P %>%
   ggplot(aes(Pset, med, colour=Paramo)) +
   geom_pointrange(aes(ymin=q5, ymax=q95),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (median with 5th and 95th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (median with 5th and 95th percentiles)")) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 P %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Paramo = as.character(Paramo)) %>%
   mutate(Paramo = recode(Paramo, "0" = "Outside", "1" = "Inside")) %>%
   filter(Pset > 48 & Pset <= 96) %>%
@@ -207,12 +209,12 @@ P %>%
   ggplot(aes(Pset, med, colour=Paramo)) +
   geom_pointrange(aes(ymin=q5, ymax=q95),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (median with 5th and 95th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (median with 5th and 95th percentiles)")) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 P %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Paramo = as.character(Paramo)) %>%
   mutate(Paramo = recode(Paramo, "0" = "Outside", "1" = "Inside")) %>%
   filter(Pset > 96) %>%
@@ -220,13 +222,13 @@ P %>%
   ggplot(aes(Pset, med, colour=Paramo)) +
   geom_pointrange(aes(ymin=q5, ymax=q95),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (median with 5th and 95th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (median with 5th and 95th percentiles)")) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 
 P %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Paramo = as.character(Paramo)) %>%
   mutate(Paramo = recode(Paramo, "0" = "Outside", "1" = "Inside")) %>%
   filter(Pset <= 48) %>%
@@ -234,12 +236,12 @@ P %>%
   ggplot(aes(Pset, mean, colour=Paramo)) +
   geom_pointrange(aes(ymin=q25, ymax=q75),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (mean with 25th and 75th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (mean with 25th and 75th percentiles)")) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 P %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Paramo = as.character(Paramo)) %>%
   mutate(Paramo = recode(Paramo, "0" = "Outside", "1" = "Inside")) %>%
   filter(Pset > 48 & Pset <= 96) %>%
@@ -247,12 +249,12 @@ P %>%
   ggplot(aes(Pset, mean, colour=Paramo)) +
   geom_pointrange(aes(ymin=q25, ymax=q75),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (mean with 25th and 75th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (mean with 25th and 75th percentiles)")) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 P %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Paramo = as.character(Paramo)) %>%
   mutate(Paramo = recode(Paramo, "0" = "Outside", "1" = "Inside")) %>%
   filter(Pset > 96) %>%
@@ -260,13 +262,13 @@ P %>%
   ggplot(aes(Pset, mean, colour=Paramo)) +
   geom_pointrange(aes(ymin=q25, ymax=q75),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (mean with 25th and 75th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (mean with 25th and 75th percentiles)")) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 
 YP %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Paramo = as.character(Paramo)) %>%
   mutate(Paramo = recode(Paramo, "0" = "Outside", "1" = "Inside")) %>%
   filter(Pset <= 48) %>%
@@ -274,13 +276,13 @@ YP %>%
   ggplot(aes(Pset, med, colour=Paramo)) +
   geom_pointrange(aes(ymin=q5, ymax=q95),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (median with 5th and 95th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (median with 5th and 95th percentiles)")) +
   facet_grid(Year~.) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 YP %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Paramo = as.character(Paramo)) %>%
   mutate(Paramo = recode(Paramo, "0" = "Outside", "1" = "Inside")) %>%
   filter(Pset > 48 & Pset <= 96) %>%
@@ -288,13 +290,13 @@ YP %>%
   ggplot(aes(Pset, med, colour=Paramo)) +
   geom_pointrange(aes(ymin=q5, ymax=q95),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (median with 5th and 95th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (median with 5th and 95th percentiles)")) +
   facet_grid(Year~.) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 YP  %>%
-  filter(Variable == "PreValue") %>%
+  filter(Variable == ltrvar) %>%
   mutate(Paramo = as.character(Paramo)) %>%
   mutate(Paramo = recode(Paramo, "0" = "Outside", "1" = "Inside")) %>%
   filter(Pset > 96) %>%
@@ -302,8 +304,8 @@ YP  %>%
   ggplot(aes(Pset, med, colour=Paramo)) +
   geom_pointrange(aes(ymin=q5, ymax=q95),
                   position = position_dodge(width = 0.75), size=0.33) +
-  ylab("PreValue") +
-  ggtitle("PreValue (median with 5th and 95th percentiles)") +
+  ylab(ltrvar) +
+  ggtitle(paste0(ltrvar," (median with 5th and 95th percentiles)")) +
   facet_grid(Year~.) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
