f5067a46e3cd370224755b073add60ca07c8710d
James Millington <jamesdamillington@gmail.com>
2020-08-12 17:29:20 +0100

    adding new plots (with wesanderson colours and common y-axis with commas)

--- NBR_plotCounts.r
+++ NBR_plotCounts.r
@@ -3,6 +3,8 @@
 library(readr)
 library(ggplot2) 
 library(dplyr)
+library(wesanderson)
+library(scales)
 
 data_dir <- paste0(getwd(),"/Data/NBRanalysis/FireCounts/")  ## define the name of directory to save results
 
@@ -36,65 +38,167 @@ count_tbl <- tibble(
 
 count_tbl <- read_csv(paste0(data_dir,"FireCounts_AllPsets.csv"))
 
+count_tbl <- count_tbl %>%
+  mutate(Window = recode(Window, All = "All Year", Third = "Jan-Apr")) %>%
+  mutate(Pset_lab = formatC(Pset, width = 3, format = "d", flag = "0")) %>%
+  mutate(Pset_lab = factor(Pset_lab))
+
+
 #for loop across parameter sets (to split plots for legibility)
 Pset_filter <- data.frame(rbind(c(0,48),c(48,96),c(96,144)))
 
-#row<-1
+
+pdf(paste0(data_dir, "FireCounts.pdf"), width=10, height=8)
+
+#barplot by pset compare Window (all fires)
+#get the data we want for this plot
+plot_dat <- count_tbl %>%
+  filter(Paramo == "All") %>%
+  group_by(Window, Pset, Pset_lab) %>%
+  summarise(Count = sum(Count))
+
+maxcount <- ceiling(max(plot_dat$Count))
+                        
+#loop over pset groups and plot
+for(row in 1:length(Pset_filter[,1])){
+  
+  lower <- Pset_filter[row,1]
+  upper <- Pset_filter[row,2]
+
+  p <- plot_dat %>%
+    filter(Pset > lower & Pset <= upper) %>%
+    ggplot(aes(Pset_lab, Count, fill=Window)) +
+    geom_bar(stat="identity", position="dodge") +
+    ylab("Fire Count") +
+    xlab("Parameter Set") +
+    ggtitle("Entire Region") +
+    scale_y_continuous(labels = comma, limits=c(0,maxcount)) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+    scale_fill_manual(values=wes_palette("Zissou1", 2, type = "discrete"))
+
+  print(p)
+} 
+
+
+
+#barplot by pset compare Window (inside paramo)
+plot_dat <- count_tbl %>%
+  filter(Paramo == "Inside") %>%
+  group_by(Window, Pset, Pset_lab) %>%
+  summarise(Count = sum(Count))
+
+maxcount <- ceiling(max(plot_dat$Count))
+
 #loop over pset groups and plot
 for(row in 1:length(Pset_filter[,1])){
   
   lower <- Pset_filter[row,1]
   upper <- Pset_filter[row,2]
   
-  #first get psets for this plot (and make pset factor)
-  subset_dat <- count_tbl %>%
-    filter(Pset > lower & Pset <= upper) %>%
-    mutate(Pset = formatC(Pset, width = 3, format = "d", flag = "0")) %>%  #pad with preceeding 0s so that plots margins are identical
-    mutate(Pset = factor(Pset)) 
-  
-  #now get the data we want for this plot
-  plot_dat <- subset_dat %>%
-    mutate(Window = recode(Window, All = "All Year", Third = "Jan-Apr")) %>%
-    filter(Paramo == "All") %>%
-    group_by(Window, Pset) %>%
-    summarise(Count = sum(Count))
-
-  maxcount <- ceiling(max(plot_dat$Count))
-
-  #barplot by pset compare Window (all fires)
   p <- plot_dat %>%
-    ggplot(aes(Pset, Count, fill=Window)) +
-    geom_bar(stat="identity", position="dodge") +
-    ylab("count ") +
-    ylim(0,maxcount) +
-    ggtitle("Total Region") +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+    filter(Pset > lower & Pset <= upper) %>%
+    ggplot(aes(Pset_lab, Count, fill=Window)) +
+    geom_bar(position="dodge", stat="identity") +
+    ylab("Fire Count") +
+    xlab("Parameter Set") +
+    ggtitle("Inside Paramo") +
+    scale_y_continuous(labels = comma, limits=c(0,maxcount)) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+    scale_fill_manual(values=wes_palette("Zissou1", 2, type = "discrete"))
 
   print(p)
-  
-  
-  
-  plot_dat <- subset_dat %>%
-    mutate(Window = recode(Window, All = "All Year", Third = "Jan-Apr")) %>%
-    #filter(Window == "All Year") %>%
-    filter(Paramo != "All") %>%
-    group_by(Window, Pset, Paramo) %>%
-    summarise(Count = sum(Count))
+} 
 
-  #barplot by pset compare Window (stack by inside vs outside)
+
+
+#barplot by pset compare Window (outside paramo)
+plot_dat <- count_tbl %>%
+  filter(Paramo == "Outside") %>%
+  group_by(Window, Pset, Pset_lab) %>%
+  summarise(Count = sum(Count))
+
+maxcount <- ceiling(max(plot_dat$Count))
+
+#loop over pset groups and plot
+for(row in 1:length(Pset_filter[,1])){
+  
+  lower <- Pset_filter[row,1]
+  upper <- Pset_filter[row,2]
+  
   p <- plot_dat %>%
-    ggplot(aes(Window, Count, fill=Paramo)) +
-    geom_bar(position="stack", stat="identity") +
-    ylab("count") +
-    #facet_grid(.~Pset) +
-    #ylim(0,maxcount) +
-    #ggtitle("Total Region") +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+    filter(Pset > lower & Pset <= upper) %>%
+    ggplot(aes(Pset_lab, Count, fill=Window)) +
+    geom_bar(position="dodge", stat="identity") +
+    ylab("Fire Count") +
+    xlab("Parameter Set") +
+    ggtitle("Outside Paramo") +
+    scale_y_continuous(labels = comma, limits=c(0,maxcount)) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+    scale_fill_manual(values=wes_palette("Zissou1", 2, type = "discrete"))
+  
+  print(p)
+}  
+  
 
+
+#barplot by pset compare Window (inside paramo) - facet by year
+plot_dat <- count_tbl %>%
+  filter(Paramo == "Inside") %>%
+  group_by(Year, Window, Pset, Pset_lab, Paramo) %>%
+  summarise(Count = sum(Count))
+
+maxcount <- ceiling(max(plot_dat$Count))
+
+#loop over pset groups and plot
+for(row in 1:length(Pset_filter[,1])){
+  
+  lower <- Pset_filter[row,1]
+  upper <- Pset_filter[row,2]
+  
+  p <- plot_dat %>%
+    filter(Pset > lower & Pset <= upper) %>%
+    ggplot(aes(Pset_lab, Count, fill=Window)) +
+    geom_bar(position="dodge", stat="identity") +
+    facet_grid(Year~.) +
+    ylab("Fire Count") +
+    xlab("Parameter Set") +
+    ggtitle("Inside Paramo") +
+    scale_y_continuous(labels = comma, limits=c(0,maxcount)) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+    scale_fill_manual(values=wes_palette("Zissou1", 2, type = "discrete"))
+  
   print(p)
   
 }
 
-# all fires, in vs out (facet on Window)
+
+#barplot by pset compare Window - facet by Paramo
+plot_dat <- count_tbl %>%
+  filter(Paramo != "All") %>%
+  group_by(Window, Pset, Pset_lab, Paramo) %>%
+  summarise(Count = sum(Count))
+
+maxcount <- ceiling(max(plot_dat$Count))
+
+#loop over pset groups and plot
+for(row in 1:length(Pset_filter[,1])){
   
+  lower <- Pset_filter[row,1]
+  upper <- Pset_filter[row,2]
+
+  p <- plot_dat %>%
+    filter(Pset > lower & Pset <= upper) %>%
+    ggplot(aes(Pset_lab, Count, fill=Window)) +
+    geom_bar(position="dodge", stat="identity") +
+    facet_grid(.~Paramo) +
+    ylab("Fire Count") +
+    xlab("Parameter Set") +
+    scale_y_continuous(labels = comma, limits=c(0,maxcount)) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+    scale_fill_manual(values=wes_palette("Zissou1", 2, type = "discrete"))
   
+  print(p)
+  
+}
+
+dev.off()
